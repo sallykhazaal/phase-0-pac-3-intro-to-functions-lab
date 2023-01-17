753722993d6d72fecbb81db441c3f609ca32c90e
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-16 20:48:06 -0500

    commit tree 798c73404e34c7e2354601de58d1182ff552ae93

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+    // todo
+  }
+  function shout(string) {
+    return string;
+  }
+  function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {}
+  function whisper(string) {return string}
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string) {}
+  function logShout(string) {
+    console.log(string.toUpperCase()) ;
+  }
+  function logWhisper(string) {}
+  function logWhisper(string) {console.log(string.toLowerCase())}
+
+  function sayHiToHeadphonedRoommate(string) {
+    const cantHear = "I can't hear you!"
+    const yesIndeed = "YES INDEED!"
+    const wouldLove = "I would love to!"
+    if (string.toLowerCase(string) === string) {return cantHear}
+    else if (string.toUpperCase(string) === string) {return yesIndeed}
+    else if (string === "Let's have dinner together!") {return wouldLove}
+  }
+  sayHiToHeadphonedRoommate("Let's have dinner together!")
